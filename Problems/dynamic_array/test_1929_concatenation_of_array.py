e03e2dd9744770c9bee22fd10b57bc560115672b
Park Joyeong <drhamst@gmail.com>
2021-09-01 09:27:30 +0900

    Change file name for naming convention

--- Problems/dynamic_array/test_1929_concatenation_of_array.py
+++ Problems/dynamic_array/test_1929_concatenation_of_array.py
@@ -1,15 +1,15 @@
 import unittest
-from . import _1929_concatenation_of_array
+from . import q_1929_concatenation_of_array
 
 
 class Test_1929(unittest.TestCase):
 
     def test_case1(self):
-        s = _1929_concatenation_of_array.Solution()
+        s = q_1929_concatenation_of_array.Solution()
         self.assertEqual(s.getConcatenation(
             [1, 2, 1]), [1, 2, 1, 1, 2, 1])
 
     def test_case2(self):
-        s = _1929_concatenation_of_array.Solution()
+        s = q_1929_concatenation_of_array.Solution()
         self.assertEqual(s.getConcatenation(
             [1, 3, 2, 1]), [1, 3, 2, 1, 1, 3, 2, 1])
--- Problems/dynamic_array/test_1_two_sum.py
+++ Problems/dynamic_array/test_1_two_sum.py
@@ -1,17 +1,17 @@
 import unittest
-from . import _1_two_sum
+from . import q_1_two_sum
 
 
 class Test_1(unittest.TestCase):
 
     def test_case1(self):
-        s = _1_two_sum.Solution()
+        s = q_1_two_sum.Solution()
         self.assertEqual(s.twoSum([2, 7, 11, 15], 9), [0, 1])
 
     def test_case2(self):
-        s = _1_two_sum.Solution()
+        s = q_1_two_sum.Solution()
         self.assertEqual(s.twoSum([3, 2, 4], 6), [1, 2])
 
     def test_case3(self):
-        s = _1_two_sum.Solution()
+        s = q_1_two_sum.Solution()
         self.assertEqual(s.twoSum([3, 3], 6), [0, 1])
