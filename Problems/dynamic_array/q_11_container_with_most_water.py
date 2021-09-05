2d7a215242c2715bd239fc0fbcdba92f4feac34a
Park Joyeong <drhamst@gmail.com>
2021-09-05 12:09:15 +0900

    Complete Q 11

--- Problems/dynamic_array/q_11_container_with_most_water.py
+++ Problems/dynamic_array/q_11_container_with_most_water.py
@@ -1,17 +1,30 @@
 from typing import List
 
+# Solution 1
+# class Solution:
+#     def maxArea(self, height: List[int]) -> int:
+#         max_water = 0
+#         for i in range(len(height)-1):
+#             for j in range(i+1, len(height)):
+#                 x = j - i
+#                 y = min(height[i], height[j])
+#                 max_water = max(max_water, x*y)
+#         return max_water
+
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        max_water = 0
-        for i in range(len(height)-1):
-            for j in range(i+1, len(height)):
-                x = j - i
-                y = 0
-                if (height[i] > height[j]):
-                    y = height[j]
-                else:
-                    y = height[i]
-                if (x*y > max_water):
-                    max_water = x*y
-        return max_water
+        l = 0
+        r = len(height) - 1
+        area = 0
+
+        while l < r:
+            # Calculating the max area
+            area = max(area, min(height[l],
+                                 height[r]) * (r - l))
+
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return area
